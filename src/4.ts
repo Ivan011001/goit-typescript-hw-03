c380d795714f0c4b0942ebd89e42a9a8bb994df5
Ivan011001 <126008836+Ivan011001@users.noreply.github.com>
2023-11-02 12:00:18 +0200

    final-commit

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,74 @@
-const key = new Key();
+class Key {
+  private signature: number;
 
-const house = new MyHouse(key);
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature(): number {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey(): Key {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+
+  public comeIn(person: Person) {
+    if (this.door) {
+      this.tenants.push(person);
+    }
+  }
+
+  public getTenants(): Person[] {
+    return this.tenants;
+  }
+
+  public abstract openDoor(key: Key): boolean;
+  public abstract closeDoor(): void;
+}
+
+class MyHouse extends House {
+  private key: Key;
+
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+
+  public openDoor(personKey: Key): boolean {
+    if (this.key.getSignature() === personKey.getSignature()) {
+      this.door = true;
+      return true;
+    }
+    return false;
+  }
+
+  public closeDoor(): void {
+    this.door = false;
+  }
+}
+
+const key = new Key();
 const person = new Person(key);
+const house = new MyHouse(key);
 
 house.openDoor(person.getKey());
-
 house.comeIn(person);
+house.getTenants(); // масив жителів з одного об'єкту person
 
-
+house.closeDoor();
+house.comeIn(person); // не додається оскільки двері зачинені
 export {};
